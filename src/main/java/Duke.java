bf71291ee36ac7f594dc0eef845cad3152655c0a
jasperosy <jasper2g@gmail.com>
2019-08-19 10:27:00 +0800

    Completed Level-1 Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,61 +1,29 @@
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import javafx.scene.layout.Region;
+import java.util.Scanner;
 
-public class Duke extends Application {
-
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-
-    public static void main(String[] args) {
-        // ...
+public class Duke {
+    public Duke () {
+        hello();
     }
-
-    @Override
-    public void start(Stage stage) {
-        //Step 1. Formatting the window to look as expected.
-
-        //...
-
-        //Step 2. Formatting the window to look as expected
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        // You will need to import `javafx.scene.layout.Region` for this.
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        // more code to be added here later
+    public void hello() {
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+    }
+    public void GreetEchoExit(String line) {
+        System.out.println(line);
+    }
+    public void SayBye() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+    public static void main(String[] args) {
+        Duke duke = new Duke();
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            String line = scan.nextLine();
+            if (line.equals("bye")) {
+                duke.SayBye();
+                break;
+            }
+            duke.GreetEchoExit(line);
+        }
     }
 }
