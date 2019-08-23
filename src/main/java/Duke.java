3d28583cf4f3572e726f533a05be5db6987dea29
jasperosy <jasper2g@gmail.com>
2019-08-23 20:41:36 +0800

    Completed Level-4 ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,30 +10,56 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
     }
-    public void GreetEchoExit(String line) {
-        System.out.println(line);
-    }
     public void SayBye() {
         System.out.println("Bye. Hope to see you again soon!");
     }
-    public void EchoAndAdd(Task line) {
-        arr.add(line);
-        System.out.println("added: " + line.getLine());
-    }
     public void ListEverything() {
+        System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < arr.size(); i++) {
-            String status = arr.get(i).getStatusIcon();
-            String line = arr.get(i).getLine();
-            System.out.println((i + 1) + ".[ " + status + " ] " + line);
+            System.out.println((i + 1) + ". " + arr.get(i));
         }
     }
     public void CompleteTask(int num) {
         arr.get(num).setStatus();
-        String status = arr.get(num).getStatusIcon();
-        String line = arr.get(num).getLine();
-        System.out.println("Nice! I've marked this task as done: \n" + "[ " + status + " ] " + line);
+        System.out.println("Nice! I've marked this task as done: \n" + arr.get(num));
+    }
+    public void listTask(Task line) {
+        System.out.println("Got it. I've added this task:");
+        System.out.println(line);
+        if (arr.size() > 1) {
+            System.out.println("Now you have " + arr.size() + " tasks in your list.");
+        }
+        else {
+            System.out.println("Now you have " + arr.size() + " task in the list.");
+        }
+    }
+    public void addToDo(String line) {
+        line = line.trim();
+        Task task = new Todo(line);
+        arr.add(task);
+        listTask(task);
+    }
+    public void addDeadline(String line) {
+        String start = line.split("/by")[0].trim();
+        String end = line.split("/by")[1].trim();
+        Task task = new Deadline(start, end);
+        arr.add(task);
+        listTask(task);
+    }
+    public void addEvent(String line) {
+        String start = line.split("/at")[0].trim();
+        String end = line.split("/at")[1].trim();
+        Task task = new Event(start, end);
+        arr.add(task);
+        listTask(task);
     }
     public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
         Duke duke = new Duke();
         Scanner scan = new Scanner(System.in);
         while (true) {
@@ -45,7 +71,7 @@ public class Duke {
             else if (line.equals("list")) {
                 duke.ListEverything();
             }
-            else if (line.split(" ")[0].equals("done")) {
+            else if (line.startsWith("done")) {
                 int num = Integer.parseInt(line.split(" ")[1]) - 1;
                 if (num < 0 || num >= duke.arr.size()) {
                     System.out.println("Invalid number!");
@@ -54,9 +80,17 @@ public class Duke {
                     duke.CompleteTask(num);
                 }
             }
-            else {
-                Task task = new Task(line);
-                duke.EchoAndAdd(task);
+            else if (line.startsWith("todo")) {
+                line = line.replaceFirst("todo", "");
+                duke.addToDo(line);
+            }
+            else if (line.startsWith("deadline")) {
+                line = line.replaceFirst("deadline", "");
+                duke.addDeadline(line);
+            }
+            else if (line.startsWith("event")) {
+                line = line.replaceFirst("event", "");
+                duke.addEvent(line);
             }
         }
     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -8,13 +8,11 @@ public class Task {
     public void setStatus() {
         this.status = true;
     }
-    public boolean status() {
-        return status;
-    }
     public String getStatusIcon() {
         return (status ? "\u2713" : "\u2718"); //return tick or X symbols
     }
-    public String getLine() {
-        return this.line;
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + line;
     }
 }
